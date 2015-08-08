c0de18b4bb2916425431f1ff10761715c908ce67
Sebastian Pipping <sebastian@pipping.org>
2015-08-09 00:31:17 +0200

    Extract parse module

---
+++ resolve_march_native/parser.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under GPL v2 or later
+
+import re
+
+
+_param_pattern = '--param [^ \\n]+'
+_simple_pattern = '-[^ \\n]+'
+
+_flag_matcher = re.compile('(?:%s|%s)' % (_param_pattern, _simple_pattern))
+
+
+def extract_flags(text):
+    start_marker_seen = False
+
+    for line in text.split('\n'):
+        if not line.startswith('#'):
+            continue
+
+        if not start_marker_seen:
+            if 'options passed' in line:
+                start_marker_seen = True
+            else:
+                continue
+
+        for m in re.finditer(_flag_matcher, line):
+            yield m.group(0)
