48c7edecb3ca6cb6ef638de96ebce03d8de5e675
Sebastian Pipping <sebastian@pipping.org>
2015-08-09 15:41:54 +0200

    Process flags that -match=<explicit> has more

--- resolve_march_native/engine.py
+++ resolve_march_native/engine.py
@@ -75,6 +75,21 @@ class Engine(object):
 	def _get_march_explicit(arch):
 		return '-march=%s' % arch
 
+	def _process_flags_explicit_has_more(self, target_set, march_native_flag_set, march_explicit_flag_set):
+		PREFIX_NO = '-mno-'
+		PREFIX_YES = '-m'
+
+		explicit_more_flag_set = march_explicit_flag_set - march_native_flag_set
+		for flag in explicit_more_flag_set:
+			if not flag.startswith('-m'):
+				print('Unsure what to do about flag %s, please report this as a bug.' % flag, file=sys.stderr)
+				continue
+
+			if not flag.startswith(PREFIX_NO) and flag.startswith(PREFIX_YES):
+				# march=<explicit> enabled something (too much) that march=native disabled
+				opposite_flag = PREFIX_NO + flag[len(PREFIX_YES):]
+				target_set.add(opposite_flag)
+
 	def _resolve(self, march_native_flag_set, march_explicit_flag_set, arch, options):
 		native_unrolled_flag_set = march_native_flag_set - march_explicit_flag_set
 		native_unrolled_flag_set.add(self._get_march_explicit(arch))
@@ -85,6 +100,11 @@ class Engine(object):
 			self._resolve_mno_flags(native_unrolled_flag_set)
 		if not options.keep_default_params:
 			self._resolve_default_params(native_unrolled_flag_set)
+
+		# NOTE: The next step needs to go after resolution of -mno-* flags
+		#       since it may add new -mno-* flags
+		self._process_flags_explicit_has_more(native_unrolled_flag_set,
+				march_native_flag_set, march_explicit_flag_set)
 
 		return native_unrolled_flag_set
 
