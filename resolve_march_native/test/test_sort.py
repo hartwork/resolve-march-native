aa5da478b23c7c2b8285a0e4ce0dceb647a8b236
Sebastian Pipping <sebastian@pipping.org>
2015-08-09 15:50:43 +0200

    Add sort regression test

---
+++ resolve_march_native/test/test_sort.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under GPL v2 or later
+
+import os
+from unittest import TestCase
+
+from resolve_march_native.sort import flags_sort_key
+
+
+class TestSort(TestCase):
+	def test_bonnell(self):
+		input_order = [
+			'--param l1-cache-size=24',
+			'-march=bonnell',
+			'-mno-cx16',
+			'--param l1-cache-line-size=64',
+		]
+		expected_order = [
+			'--param l1-cache-line-size=64',
+			'--param l1-cache-size=24',
+			'-march=bonnell',
+			'-mno-cx16',
+		]
+		actual_order = sorted(input_order, key=flags_sort_key)
+		self.assertEquals(actual_order, expected_order)
