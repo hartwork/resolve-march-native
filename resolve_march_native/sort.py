728a3ff7dee8328eec885222e60a8971779e03fc
Sebastian Pipping <sebastian@pipping.org>
2015-08-21 19:40:16 +0200

    Make "-O2 -pipe" go last

--- resolve_march_native/sort.py
+++ resolve_march_native/sort.py
@@ -5,6 +5,10 @@ def flags_sort_key(text):
 	if text.startswith('-march='):
 		return (0, text)  # March goes first
 	elif text.startswith('--param'):
-		return (2, text)  # --param goes last
+		return (2, text)
+	elif text.startswith('-O'):
+		return (3, text)
+	elif text == '-pipe':
+		return (4, text)
 	else:
 		return (1, text)
