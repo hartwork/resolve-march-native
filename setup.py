250e5861d573171663ef96f3bf203973d9a60577
Sebastian Pipping <sebastian@pipping.org>
2023-01-09 20:16:16 +0100

    Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3 :: Only',
             'Topic :: Software Development :: Compilers',
             'Topic :: Utilities',
