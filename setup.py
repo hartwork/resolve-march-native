7d30f049e76038b2cb052364376fb0340ea6e04c
Sebastian Pipping <sebastian@pipping.org>
2025-10-15 00:12:37 +0200

    Drop support for Python <=3.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         author='Sebastian Pipping',
         author_email='sebastian@pipping.org',
         url='https://github.com/hartwork/resolve-march-native',
-        python_requires='>=3.9',
+        python_requires='>=3.10',
         setup_requires=[
             'setuptools>=38.6.0',  # for long_description_content_type
         ],
@@ -43,7 +43,6 @@ if __name__ == '__main__':
             'Operating System :: POSIX :: Linux',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.12',
